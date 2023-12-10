6e0ab0c719a1bfc5a66ecb18cc320b8b7afba56d
Steve Pikelny <spike716@gmail.com>
2023-12-10 12:54:52 -0500

    bleh

--- docs/chats/DrAndy.js
+++ docs/chats/DrAndy.js
@@ -1,4 +1,13 @@
 /*
+
   - if user mentions credits or insurance: sorry, i don't accept credits/insurance
+
+
+
+
+
+
+description: Do you feel stuck? Has life lost its luster?
+
 
 */
--- docs/state/all.js
+++ docs/state/all.js
@@ -6,7 +6,7 @@ export * from '../chats/samanthaJones.js'
 export * from '../chats/heatherHot.js'
 export * from '../chats/cagla.js'
 export * from '../chats/katFischer.js'
-export * from '../chats/vinceSlickson.js'
+export * from '../chats/VinceSlickson.js'
 
 import {ls} from '../$.js'
 
